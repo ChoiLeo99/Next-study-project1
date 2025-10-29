ed4dad11e98f42206ea82aa3716accd29e3a2acd
ChoiLeo99 <choileo0910@gmail.com>
2025-10-30 00:58:02 +0900

    프로젝트 페이지 세팅중

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+import React from 'react'
+
+export default function DashBoardPage() {
+  return (
+    // <h1>About usasas!</h1>
+    <div style={{padding:"20px", width:"20px",height:"100px", background:"#ff2312"}}></div>
+  )
+}
