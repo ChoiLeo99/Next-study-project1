7e2266d901b3f686f04eb5b3c666e7f987116d9e
ChoiLeo99 <choileo0910@gmail.com>
2025-10-31 22:59:48 +0900

    generateStaticParams 사용, Not Found 페이지 생성

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFoundPage() {
+  return (
+    <div>
+        <h2>페이지를 찾을 수 없음 ㅠㅠ 😊</h2>
+
+    </div>
+  )
+}
--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default async function ModelPage({
+export default async function PantsPage({
   params,
 }: {
   params: Promise<{ slug: string }>
@@ -11,3 +11,9 @@ export default async function ModelPage({
   return <h1>{slug} 제품 설명 페이지</h1>
 }
 
+export function generateStaticParams() {
+  const products = ['pants', 'skirt'];
+  return products.map(products => ({
+    slug: products,
+  }))
+}
