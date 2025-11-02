f2124fd142b631cf1cf946583553f87a3f997a23
ChoiLeo99 <choileo0910@gmail.com>
2025-11-02 16:31:50 +0900

    메타데이타 기능 추가

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,27 +1,41 @@
-import React from 'react'
+import type { Metadata, ResolvingMetadata } from 'next';
+import React from 'react';
 
-export default async function PantsPage({
-  params,
-}: {
-  params: Promise<{ slug: string }>
-}) {
-  const { slug } = await params
+type Props = {
+  params: Promise<{ slug: string }>;
+};
 
-  console.log("서버에서 받은 경로 위치:", slug)
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const { slug } = await params;
+  return {
+    title: `제품의 이름: ${slug}`,
+    description: `${slug} 제품의 상세 설명 페이지입니다.`,
+    icons: {
+      icon: '/favicon.ico'
+    }
+  };
+}
+
+export default async function PantsPage({ params }: Props) {
+  const { slug } = await params;
+
   return (
     <div>
       <header>
-        <a href=''>남성복</a>
-        <a href=''>여성복</a>
+        <a href="">남성복</a>
+        <a href="">여성복</a>
       </header>
-      <h1> {slug} 제품 설명 페이지</h1>
+      <h1>{slug} 제품 설명 페이지</h1>
     </div>
   );
 }
 
 export function generateStaticParams() {
   const products = ['pants', 'skirt'];
-  return products.map(products => ({
-    slug: products,
-  }))
+  return products.map((product) => ({
+    slug: product,
+  }));
 }
